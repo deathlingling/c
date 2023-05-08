0e6bf3b200c788f1644ca1aaf53d3fb1bb90e46d
Deathlingling <113222346+deathlingling@users.noreply.github.com>
2023-05-08 14:59:26 +0800

    Add files via upload
    5.8

---
+++ main4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "string.h"
+int main() {
+    int a[6]={0,1,0,0,0,0};
+    float b[10]={1.2,0,1.3};
+    char c[10]={"china"};
+    char c1[10]={'c','h','i','n','a'};
+    char c2[10];
+    char str[]="你好，世界！";
+//    char str1[15],str2[]="你好，C！";
+//    strcpy(str1,str2);
+//    puts(str1);
+//    scanf("%s",c2);
+//    printf("%s",c2);
+//    for (int i = 0; i < 10; ++i) {
+//        scanf("%c",&c2[i]);
+//    }
+//    for (int i = 0; i < 10; ++i) {
+//        printf("%c",c2[i]);
+//    }
+//    gets(c2);
+//    puts(c2);
+
+    puts(str);
+
+
+//    printf("%f",b[0]);
+    return 0;
+}
